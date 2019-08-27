18ae72a81ea61402a1267a14d8647599fbee8ad7
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 13:54:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  monopoly_with_second_tier
+  hash = monopoly_with_second_tier
 end
 
 def monopoly_with_fourth_tier
