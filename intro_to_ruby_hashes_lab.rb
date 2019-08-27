3cffc29a41cbf7621795ce506ea7541e84611b32
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 14:08:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,6 +24,7 @@ def monopoly_with_third_tier
   
   hash = monopoly_with_second_tier
   hash[:railroads][:rent_in_dollars] = {}
+  hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
   hash
 end
 
