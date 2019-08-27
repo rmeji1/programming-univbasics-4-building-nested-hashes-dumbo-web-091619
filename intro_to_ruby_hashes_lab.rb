227fcf07b049f3850ec37449b8f81216f6f7ee2b
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 14:14:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,8 @@ def monopoly_with_third_tier
   hash[:railroads][:names] = {
     reading_railroad: 
     pennsylvania_railroad:
-b_and_o_railroad
-shortline_railroad
+    b_and_o_railroad:
+    shortline_railroad: 
   }
   
   
