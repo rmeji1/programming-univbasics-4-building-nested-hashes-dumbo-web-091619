3305bed5512f24b36e8c2c26c8d22e4cfba7413d
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 13:49:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,6 +10,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   base_hash = base_hash
+  base_hash[:railroads]
 end
 
 def monopoly_with_third_tier
