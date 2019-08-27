8281ce6d9fd26fc57b2473923f0a087fb3bcb4cd
Roberto Mejia <rmeji1@unh.newhaven.edu>
2019-08-27 14:05:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
   # complexity.
   
   hash = monopoly_with_second_tier
-  hash[:railroads][:rent_in]
+  hash[:railroads][:rent_in_dollars] = 4
 end
 
 def monopoly_with_fourth_tier
